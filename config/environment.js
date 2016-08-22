806afb225427afd3436b85bd5ee1ec4ff682b1a6
Anand Sakthivel <anandts88@gmail.com>
2016-08-21 22:30:13 -0400

    Changed app name

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'my-mirage',
+    modulePrefix: 'ember-data-mirage',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
